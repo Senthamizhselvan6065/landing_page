73a6f82a9b69a9da78dc340e711b786da91d963b
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-09 09:56:19 +0530

    style : all components are updated now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import WorkFlow from './components/WorkFlow';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -8,6 +13,11 @@ const App = () => {
         <Navbar />
         <div className="max-w-7xl mx-auto pt-20 px-6">
             <HeroSection />
+            <FeatureSection />
+            <WorkFlow />
+            <Pricing />
+            <Testimonials />
+            <Footer />
         </div>
     </div>
   )
